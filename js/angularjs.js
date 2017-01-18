6054595cb3b507ed391171965eb1ac44cef65dda
aymericboccard <aymeric.boccard@etu.emse.fr>
2017-01-18 20:57:02 +0100

    fix: new API is used instead of Google feed API

--- js/angularjs.js
+++ js/angularjs.js
@@ -97,13 +97,15 @@ myApp
         var type = ""; // type de l'article our l'affichage, s'il doit être affiché petit, grand ou moyen.
         var now = new Date();
         var intervalle = 0; // nbr de secondes entre maintenant et la date de l'aricle
+        var month = ""; // pour ecrire 01 au lieu de 1
         
         
         for(var src in sources){
             if(src != "sites"){
                 for(var url in sources[src]){
-                    var feed = new google.feeds.Feed(sources[src][url]);
-                    feed.load(function(result, src) {
+                    // API changé
+                    feednami.loadGoogleFormat(sources[src][url],
+                    function(result, src) {
                         if (!result.error) {
                             for (var i = 0; i < result.feed.entries.length; i++) {
                                 
@@ -111,7 +113,7 @@ myApp
                                 date = result.feed.entries[i].publishedDate;
                                 
                                 //on détermine le type de l'article en fonction de la date
-                                intervalle = (now.getFullYear()-date.substring(12,16))*86400*365+(now.getMonth()+1-getMonthFromString(date.substring(8,11)))*86400*30+(now.getDate()-date.substring(5,7))*86400+(now.getHours()-date.substring(17,19))*3600+(now.getMinutes()-date.substring(20,22))*60+(now.getSeconds()-date.substring(23,25));
+                                intervalle = (now.getFullYear()-date.substring(7,11))*86400*365+(now.getMonth()+1-getMonthFromString(date.substring(3,6)))*86400*30+(now.getDate()-date.substring(0,2))*86400+(now.getHours()-date.substring(12,14))*3600+(now.getMinutes()-date.substring(15,17))*60+(now.getSeconds()-date.substring(18,20));
                                 
                                 
                                 if(intervalle<12*3600){
@@ -122,11 +124,12 @@ myApp
                                     type = "articles-small";
                                 }
                                 
+                                month = getMonthFromString(date.substring(3,6))<10 ? "0"+getMonthFromString(date.substring(3,6)):""+getMonthFromString(date.substring(3,6));
+                                
                                 //construction de le date de l'article. On ne peut pas a prendre telle qu'elle est car cela pose problème pour le tri
-                                date = date.substring(12,16)+"-"+getMonthFromString(date.substring(8,11))+"-"+date.substring(5,7)+" "+date.substring(17,25);
+                                date = date.substring(7,11)+"-"+month+"-"+date.substring(0,2)+" "+date.substring(12,20);
                                 
                                 //met en majuscule + enregistrement dans scope.allCategories + ajout ou non de l'article
-                                //alert(src);
                                 for(var element in result.feed.entries[i].categories){
                                     result.feed.entries[i].categories[element] = result.feed.entries[i].categories[element].toUpperCase();
                                     scope.allCategories.includes(result.feed.entries[i].categories[element]) ? true : scope.allCategories.push(result.feed.entries[i].categories[element]);
@@ -136,7 +139,6 @@ myApp
                                 scope.articles.push({"title":result.feed.entries[i].title, "contentSnippet":result.feed.entries[i].contentSnippet, "link":result.feed.entries[i].link, "date":date, "type":type, "categories":result.feed.entries[i].categories});
                             }
                         }
-                        //scope.allCategories.sort();
                         $scope.$apply();
                     });
                 }
